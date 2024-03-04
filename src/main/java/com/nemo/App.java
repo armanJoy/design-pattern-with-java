107c37a5d2aad620a791cbc9b458762dd424f5e7
armanjoy <arman.reza@technonext.com>
2024-03-04 12:21:57 +0600

    initiated maven project

---
+++ src/main/java/com/nemo/App.java
@@ -0,0 +1,13 @@
+package com.nemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
